4ae08115732afc4c96cd97a08548ded83dd70959
Mari <weidner@cs.uni-freiburg.de>
2023-06-09 01:31:39 +0200

    change presence

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import functools
 import traceback
-from discord import ButtonStyle, Client, Colour, Embed, Forbidden, Intents, Interaction, NotFound, Role, utils
+from discord import ButtonStyle, Client, Colour, Embed, Forbidden, Game, Intents, Interaction, NotFound, Role, utils
 from discord.ui import View, Button, Modal, TextInput
 
 from .env import env
@@ -26,6 +26,7 @@ class Bot(Client):
     super().__init__(intents=intents, **options)
     
   async def on_ready(self):
+    await self.change_presence(activity=Game(name=f"{datetime.now()}"))
     await self.authentication()
     
 
@@ -33,7 +34,7 @@ class Bot(Client):
   async def authentication(self):
     # get login channel
     chan = self.get_channel(int(env.discord_auth_channel))
-    print(1)
+
     # add message if not exists
     try:
       msg = await chan.fetch_message(chan.last_message_id)
@@ -85,7 +86,7 @@ class Bot(Client):
           else:
             # send failure message
             await interaction.response.send_message("<token not found>", ephemeral=True)
-    print(2)
+    
     # starts the authentication process
     @try_catch
     async def start_authentication(interaction: Interaction):
@@ -104,13 +105,13 @@ class Bot(Client):
     authenticate_button.callback = start_authentication 
 
     view = View(timeout=None).add_item(authenticate_button)
-    print(3)
+
     embed = Embed(type="rich", title="Login using University Account", url="http://127.0.0.1:8000/auth/login", colour=Colour.blue(), timestamp=datetime.now(), description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vehicula pulvinar urna quis hendrerit. In hendrerit odio ac molestie sagittis. In fermentum nulla ac fringilla finibus. Fusce non mi porta, cursus urna id, tempor nibh. Morbi vitae turpis iaculis, imperdiet ex vitae, rhoncus ex. Phasellus congue odio eget pellentesque sagittis. Donec metus enim, molestie sit amet rutrum quis, vehicula eget diam.")
    
     embed.add_field(name="1. Login using Web", value="Donec sapien turpis, aliquet sit amet magna quis, ornare ullamcorper est. Morbi pharetra suscipit ex, vel feugiat tortor facilisis quis. Pellentesque nec leo in lacus malesuada varius ut eu erat. Nam dignissim aliquam orci, non lobortis quam imperdiet sollicitudin. Sed dapibus vulputate purus quis tincidunt. Sed non ipsum eget nibh hendrerit gravida a ac nibh. Cras ut tempor elit.", inline=False)
     embed.add_field(name="2. Enter Token", value="Vestibulum et consequat dolor, tincidunt molestie odio. Maecenas orci elit, pulvinar vel lorem vitae, tristique feugiat libero. Sed sit amet purus vitae lectus porttitor dignissim. Fusce lacinia augue turpis, vel ullamcorper ante ultricies eget. Curabitur vulputate ornare quam, eu gravida orci aliquet a. Pellentesque eget mi mi. Donec sollicitudin cursus velit, vel aliquam risus vehicula quis. Nulla lacinia enim a nibh malesuada, a imperdiet nulla imperdiet. Aliquam rutrum pulvinar purus, in porttitor turpis interdum vel.", inline=False)
     embed.set_footer(text="Login powered by Laurel")
     embed.set_image(url="https://cd.uni-freiburg.de/app/uploads/2023/02/UFR-vorlage-designsystem-typo-farben-V1.96.png")
-    print(4)
+
     await msg.edit(content="", embed=embed, view=view)
    
