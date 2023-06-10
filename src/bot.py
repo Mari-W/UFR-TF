728a7d91db47321652dcf9a289fdf48ae271a4a3
Mari <weidner@cs.uni-freiburg.de>
2023-06-10 19:46:08 +0200

    fix indentation

--- src/bot.py
+++ src/bot.py
@@ -13,12 +13,30 @@ class Bot(Client):
     super().__init__(intents=intents, **options)
     
   async def on_ready(self):
+    await self.about()
     await self.authentication()
     await self.account()
 
+  async def about(self):
+    # get about channel
+    chan = utils.get(self.guilds[0].text_channels, name="about")
+   
+    # add message if not exists
+    try:
+      msg = await chan.fetch_message(chan.last_message_id)
+    except NotFound:
+      msg = await chan.send(content="...")
+
+    embed = Embed(type="rich", title="About this Discord Server", colour=Colour.blue(), timestamp=datetime.now(), description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vehicula pulvinar urna quis hendrerit. In hendrerit odio ac molestie sagittis. In fermentum nulla ac fringilla finibus. Fusce non mi porta, cursus urna id, tempor nibh. Morbi vitae turpis iaculis, imperdiet ex vitae, rhoncus ex. Phasellus congue odio eget pellentesque sagittis. Donec metus enim, molestie sit amet rutrum quis, vehicula eget diam.")
+   
+    embed.add_field(name="1. Orga", value="Donec sapien turpis, aliquet sit amet magna quis, ornare ullamcorper est. Morbi pharetra suscipit ex, vel feugiat tortor facilisis quis. Pellentesque nec leo in lacus malesuada varius ut eu erat. Nam dignissim aliquam orci, non lobortis quam imperdiet sollicitudin. Sed dapibus vulputate purus quis tincidunt. Sed non ipsum eget nibh hendrerit gravida a ac nibh. Cras ut tempor elit.", inline=False)
+    embed.add_field(name="2. Rules", value="Vestibulum et consequat dolor, tincidunt molestie odio. Maecenas orci elit, pulvinar vel lorem vitae, tristique feugiat libero. Sed sit amet purus vitae lectus porttitor dignissim. Fusce lacinia augue turpis, vel ullamcorper ante ultricies eget. Curabitur vulputate ornare quam, eu gravida orci aliquet a. Pellentesque eget mi mi. Donec sollicitudin cursus velit, vel aliquam risus vehicula quis. Nulla lacinia enim a nibh malesuada, a imperdiet nulla imperdiet. Aliquam rutrum pulvinar purus, in porttitor turpis interdum vel.", inline=False)
+
+    await msg.edit(content="", embed=embed)
+
   async def authentication(self):
     # get login channel
-    chan = utils.get(self.guilds[0].text_channels, name="authentication")
+    chan = utils.get(self.guilds[0].text_channels, name="authenticate")
    
     # add message if not exists
     try:
@@ -141,7 +159,7 @@ async def login(token: str, interaction: Interaction):
       role = utils.get(interaction.guild.roles, name=name)
       if role is None:
         role = await interaction.guild.create_role(name=name, colour = Colour.from_rgb(255, 255, 255))
-        return role
+      return role
             
     # assign according roles
     studies = mapping.get(user["studies"], user["studies"]) if user["studies"] is not None else "Employee"
@@ -154,7 +172,8 @@ async def login(token: str, interaction: Interaction):
     except Forbidden:
       # user is server owner
       pass
-
+    
+    print(4)
     # send success message
     await interaction.response.send_message(f"<logged in as {user['sub']}>", ephemeral=True)
   # token is invalid
